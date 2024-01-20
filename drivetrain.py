161c711704898f5a9333a9daea5f429e34ddb960
ppalaganas <ppalaganas@gmail.com>
2024-01-20 16:08:30 -0500

    Add files via upload

---
+++ drivetrain.py
@@ -0,0 +1,160 @@
+#
+# Copyright (c) FIRST and other WPILib contributors.
+# Open Source Software; you can modify and/or share it under the terms of
+# the WPILib BSD license file in the root directory of this project.
+#
+
+import math
+import wpilib
+import wpimath.geometry
+import wpimath.kinematics
+import swervemodule
+
+kMaxSpeed = 3.0  # 3 meters per second
+kMaxAngularSpeed = math.pi  # 1/2 rotation per second
+
+
+class Drivetrain:
+    """
+    Represents a swerve drive style drivetrain.
+    """
+
+    def __init__(self) -> None:
+        # NOTE: May need to tweak the center measurements 44.5mm rough measurement
+        # NOTE: EVERYTHING IS MEASURE IN METERS! 
+
+        self.frontLeftLocation = wpimath.geometry.Translation2d(0.445, 0.445)
+        self.frontRightLocation = wpimath.geometry.Translation2d(0.445, -0.445)
+        self.backLeftLocation = wpimath.geometry.Translation2d(-0.445, 0.445)
+        self.backRightLocation = wpimath.geometry.Translation2d(-0.445, -0.445)
+
+        self.frontLeft = swervemodule.SwerveModule(4, 3, 4, 13)
+        self.frontRight = swervemodule.SwerveModule(7, 8, 7, 10)
+        self.backLeft = swervemodule.SwerveModule(2, 1, 2, 11)
+        self.backRight = swervemodule.SwerveModule(5, 6, 5, 12)
+
+        '''
+        BERT NOTES:
+        
+        driveMotorID: int,
+        turningMotorID: int,
+        driveEncoderID: int,
+        turningEncoderID: int,
+
+        Front Left (+,+) 
+            - Drive Motor: 4
+            - Rotation Motor: 3
+            - Drive Encoder: 4
+            - Rotation Encoder: 13 
+        
+        Front Right (+,-) 
+            - Drive Motor: 7
+            - Rotation Motor: 8
+            - Drive Encoder: 7
+            - Rotation Encoder: 10
+
+        Rear Left (-,+) 
+            - Drive Motor: 2
+            - Rotation Motor: 1
+            - Drive Encoder: 2
+            - Rotation Encoder: 11
+
+        Rear Right (-,-) 
+            - Drive Motor: 5
+            - Rotation Motor: 6
+            - Drive Encoder: 5
+            - Rotation Encoder: 12
+            
+        # Drive Motors
+        self.frontLeftModule_driveMotor = CANSparkMax(4, CANSparkMax.MotorType.kBrushless)
+        self.frontRightModule_driveMotor = CANSparkMax(7, CANSparkMax.MotorType.kBrushless)
+        self.rearLeftModule_driveMotor = CANSparkMax(2, CANSparkMax.MotorType.kBrushless)
+        self.rearRightModule_driveMotor = CANSparkMax(5, CANSparkMax.MotorType.kBrushless)
+        
+        # Rotate Motors
+        self.frontLeftModule_rotateMotor = CANSparkMax(3, CANSparkMax.MotorType.kBrushless)
+        self.frontRightModule_rotateMotor = CANSparkMax(8, CANSparkMax.MotorType.kBrushless)
+        self.rearLeftModule_rotateMotor = CANSparkMax(1, CANSparkMax.MotorType.kBrushless)
+        self.rearRightModule_rotateMotor = CANSparkMax(6, CANSparkMax.MotorType.kBrushless)
+
+        # Rotation Encoders
+        self.frontLeftModule_encoder = CANCoder(13, "rio")
+        self.frontRightModule_encoder = CANCoder(10, "rio")
+        self.rearLeftModule_encoder = CANCoder(11, "rio")
+        self.rearRightModule_encoder = CANCoder(12, "rio")
+
+        # Drive Encoders - check if we need absolute or relative encoder
+        self.frontLeftModule_drive_encoder = SparkMaxAbsoluteEncoder(4)
+        self.frontRightModule_drive_encoder = SparkMaxAbsoluteEncoder(7)
+        self.rearLeftModule_drive_encoder = SparkMaxAbsoluteEncoder(2)
+        self.rearRightModule_drive_encoder = SparkMaxAbsoluteEncoder(5)
+
+        '''
+
+        self.gyro = wpilib.AnalogGyro(0)
+
+        self.kinematics = wpimath.kinematics.SwerveDrive4Kinematics(
+            self.frontLeftLocation,
+            self.frontRightLocation,
+            self.backLeftLocation,
+            self.backRightLocation,
+        )
+
+        self.odometry = wpimath.kinematics.SwerveDrive4Odometry(
+            self.kinematics,
+            self.gyro.getRotation2d(),
+            (
+                self.frontLeft.getPosition(),
+                self.frontRight.getPosition(),
+                self.backLeft.getPosition(),
+                self.backRight.getPosition(),
+            ),
+        )
+
+        self.gyro.reset()
+
+    def drive(
+        self,
+        xSpeed: float,
+        ySpeed: float,
+        rot: float,
+        fieldRelative: bool,
+        periodSeconds: float,
+    ) -> None:
+        """
+        Method to drive the robot using joystick info.
+        :param xSpeed: Speed of the robot in the x direction (forward).
+        :param ySpeed: Speed of the robot in the y direction (sideways).
+        :param rot: Angular rate of the robot.
+        :param fieldRelative: Whether the provided x and y speeds are relative to the field.
+        :param periodSeconds: Time
+        """
+        swerveModuleStates = self.kinematics.toSwerveModuleStates(
+            wpimath.kinematics.ChassisSpeeds.discretize(
+                wpimath.kinematics.ChassisSpeeds.fromFieldRelativeSpeeds(
+                    xSpeed, ySpeed, rot, self.gyro.getRotation2d()
+                )
+                if fieldRelative
+                else wpimath.kinematics.ChassisSpeeds(xSpeed, ySpeed, rot),
+                periodSeconds,
+            )
+        )
+        wpimath.kinematics.SwerveDrive4Kinematics.desaturateWheelSpeeds(
+            swerveModuleStates, kMaxSpeed
+        )
+        self.frontLeft.setDesiredState(swerveModuleStates[0])
+        self.frontRight.setDesiredState(swerveModuleStates[1])
+        self.backLeft.setDesiredState(swerveModuleStates[2])
+        self.backRight.setDesiredState(swerveModuleStates[3])
+
+    def updateOdometry(self) -> None:
+        """Updates the field relative position of the robot."""
+        self.odometry.update(
+            self.gyro.getRotation2d(),
+            (
+                self.frontLeft.getPosition(),
+                self.frontRight.getPosition(),
+                self.backLeft.getPosition(),
+                self.backRight.getPosition(),
+            ),
+        )
--- robot.py
+++ robot.py
@@ -1,209 +1,68 @@
 #!/usr/bin/env python3
+#
+# Copyright (c) FIRST and other WPILib contributors.
+# Open Source Software; you can modify and/or share it under the terms of
+# the WPILib BSD license file in the root directory of this project.
+#
 
 import wpilib
-
-from ctre.sensors import CANCoder
-from rev import CANSparkMax, SparkMaxAbsoluteEncoder
-
-from magicbot import MagicRobot
-
-from robotpy_ext.autonomous.selector import AutonomousModeSelector
-
-from networktables import NetworkTables
-from networktables.util import ntproperty
-
-#from rev.color import ColorSensorV3, ColorMatch
-
-from components import swervedrive, swervemodule
-#from common import color_sensor, vision
-
-from collections import namedtuple
-# Get the config preset from the swervemodule
-ModuleConfig = swervemodule.ModuleConfig
-
-import time
-
-class MyRobot(MagicRobot):
-    """
-    After creating low-level components like "shooter", use component's name and an underscore
-    to inject objects to the component.
-    
-    e.g.
-    Using variable annotation like "shooter_beltMotor: ctre.WPI_VictorSPX" decleares the type of the variable.
-    When beltMotor is called from the shooter component, it's going to be a VictorSPX object.
-
-    Using equal sign for variable decleration like "shooter_beltMotor = ctre.WPI_VictorSPX(11)" creates the actual object.
-    When beltMotor is called from the shooter component, it's going to be a VictorSPX at the can port 11.
-
-    Use the equal sign (mostly) in the #createObjects function so they can be correctly injected to their parent components.
-
-    For more info refer to https://robotpy.readthedocs.io/en/stable/frameworks/magicbot.html
-    """
-
-    # Create low-level object
-    drive: swervedrive.SwerveDrive
-
-    frontLeftModule: swervemodule.SwerveModule
-    frontRightModule: swervemodule.SwerveModule
-    rearLeftModule: swervemodule.SwerveModule
-    rearRightModule: swervemodule.SwerveModule
-
-    # Create configs for each module. This is before #createObjects because modules need these configs to be initialized.
-    frontLeftModule_cfg = ModuleConfig(sd_prefix='FrontLeft_Module', zero=202.67578125, inverted=True, allow_reverse=True)
-    frontRightModule_cfg = ModuleConfig(sd_prefix='FrontRight_Module', zero=273.8671875, inverted=False, allow_reverse=True)
-    rearLeftModule_cfg = ModuleConfig(sd_prefix='RearLeft_Module', zero=232.822265625, inverted=True, allow_reverse=True)
-    rearRightModule_cfg = ModuleConfig(sd_prefix='RearRight_Module', zero=5.2734375, inverted=False, allow_reverse=True)
-
-    # Create common components
-    #vision: vision.Vision
-    #colorSensor: color_sensor.ColorSensor
-
-    def createObjects(self):
-        """
-        This is where all the components are actually created with "=" sign.
-        Components with a parent prefix like "shooter_" will be injected.
-        """
-        # SmartDashboard
-        self.sd = NetworkTables.getTable('SmartDashboard')
-
-        # Gamepad
-        self.gamempad = wpilib.Joystick(2)
-        #self.gamempad2 = wpilib.Joystick(1)
-
-        # Drive Motors
-        self.frontLeftModule_driveMotor = CANSparkMax(4, CANSparkMax.MotorType.kBrushless)
-        self.frontRightModule_driveMotor = CANSparkMax(7, CANSparkMax.MotorType.kBrushless)
-        self.rearLeftModule_driveMotor = CANSparkMax(2, CANSparkMax.MotorType.kBrushless)
-        self.rearRightModule_driveMotor = CANSparkMax(5, CANSparkMax.MotorType.kBrushless)
-        
-        # Rotate Motors
-        self.frontLeftModule_rotateMotor = CANSparkMax(3, CANSparkMax.MotorType.kBrushless)
-        self.frontRightModule_rotateMotor = CANSparkMax(8, CANSparkMax.MotorType.kBrushless)
-        self.rearLeftModule_rotateMotor = CANSparkMax(1, CANSparkMax.MotorType.kBrushless)
-        self.rearRightModule_rotateMotor = CANSparkMax(6, CANSparkMax.MotorType.kBrushless)
-
-        # Encoders
-        self.frontLeftModule_encoder = CANCoder(13, "rio")
-        self.frontRightModule_encoder = CANCoder(10, "rio")
-        self.rearLeftModule_encoder = CANCoder(11, "rio")
-        self.rearRightModule_encoder = CANCoder(12, "rio")
-
-        # PDP
-        self.pdp = wpilib.PowerDistribution(0, wpilib._wpilib.PowerDistribution.ModuleType.kCTRE)
-
-    def disabledPeriodic(self):
-        # Update the dashboard, even when the robot is disabled.
-        self.update_sd()
+import wpimath
+import wpilib.drive
+import wpimath.filter
+import wpimath.controller
+import drivetrain
 
 
-    def autonomousInit(self):
-        # Reset the drive when the auto starts.
-        #self.drive.flush()
-        #self.drive.threshold_input_vectors = True
-        pass
+class MyRobot(wpilib.TimedRobot):
+    def robotInit(self) -> None:
+        """Robot initialization function"""
+        self.controller = wpilib.Joystick(2)
+        #self.controller = wpilib.XboxController(0)
+        self.swerve = drivetrain.Drivetrain()
 
-    def autonomous(self):
-        # For auto, use MagicBot's auto mode.
-        # This will load the ./autonomous folder.
-        #super().autonomous()
-        pass
+        # Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0 to 1.
+        self.xspeedLimiter = wpimath.filter.SlewRateLimiter(3)
+        self.yspeedLimiter = wpimath.filter.SlewRateLimiter(3)
+        self.rotLimiter = wpimath.filter.SlewRateLimiter(3)
 
-    def teleopInit(self):
-        # Reset the drive when the teleop starts.
-        self.drive.flush()
-        self.drive.squared_inputs = True
-        self.drive.threshold_input_vectors = True
-        print("in teleop init")
+    def autonomousPeriodic(self) -> None:
+        self.driveWithJoystick(False)
+        self.swerve.updateOdometry()
 
-    def move(self, x, y, rcw):
-        """
-        This function is ment to be used by the teleOp.
-        :param x: Velocity in x axis [-1, 1]
-        :param y: Velocity in y axis [-1, 1]
-        :param rcw: Velocity in z axis [-1, 1]
-        """
+    def teleopPeriodic(self) -> None:
+        self.driveWithJoystick(True)
 
-        if self.gamempad.getRawButton(3):
-            # If the button is pressed, lower the rotate speed.
-            rcw *= 0.7
-        
-        self.drive.move(x, y, rcw)
+    def driveWithJoystick(self, fieldRelative: bool) -> None:
+        # Get the x speed. We are inverting this because Xbox controllers return
+        # negative values when we push forward.
+        # NOTE: Check if we need inversion here
+        xSpeed = (
+            -self.xspeedLimiter.calculate(
+                wpimath.applyDeadband(self.controller.getRawAxis(1), 0.02)
+            )
+            * drivetrain.kMaxSpeed
+        )
 
-    def teleopPeriodic(self):
-        # Drive
-        self.move(self.gamempad.getRawAxis(1), self.gamempad.getRawAxis(2), self.gamempad.getRawAxis(5))
+        # Get the y speed or sideways/strafe speed. We are inverting this because
+        # we want a positive value when we pull to the left. Xbox controllers
+        # return positive values when you pull to the right by default.
+        # NOTE: Check if we need inversion here
+        ySpeed = (
+            -self.yspeedLimiter.calculate(
+                wpimath.applyDeadband(self.controller.getRawAxis(5), 0.02)
+            )
+            * drivetrain.kMaxSpeed
+        )
 
-        print(self.frontRightModule_encoder.getAbsolutePosition())
-        #print(self.gamempad.getRawAxis(2))
-        '''
-        # Lock
-        if self.gamempad.getRawButton(1):
-            self.drive.request_wheel_lock = True
+        # Get the rate of angular rotation. We are inverting this because we want a
+        # positive value when we pull to the left (remember, CCW is positive in
+        # mathematics). Xbox controllers return positive values when you pull to
+        # the right by default.
+        rot = (
+            -self.rotLimiter.calculate(
+                wpimath.applyDeadband(self.controller.getRawAxis(2), 0.02)
+            )
+            * drivetrain.kMaxSpeed
+        )
 
-        # Vectoral Button Drive
-        if self.gamempad.getPOV() == 0:
-            self.drive.set_raw_fwd(-0.35)
-        elif self.gamempad.getPOV() == 180:
-            self.drive.set_raw_fwd(0.35)
-        elif self.gamempad.getPOV() == 90:
-            self.drive.set_raw_strafe(0.35)
-        elif self.gamempad.getPOV() == 270:
-            self.drive.set_raw_strafe(-0.35)
-
-        # Climber
-        if self.gamempad2.getRawButton(1):
-            self.climbingMotor.set(1)
-        else:
-            self.climbingMotor.set(0)
-
-        # Hook
-        if self.gamempad2.getRawAxis(5) < 0 and not self.switch.get():
-            self.hookMotor.set(self.gamempad2.getRawAxis(5))
-        elif self.gamempad2.getRawAxis(5) > 0:
-            self.hookMotor.set(self.gamempad2.getRawAxis(5))
-        else:
-            self.hookMotor.set(0)
-
-        # Shooter
-        if self.gamempad.getRawAxis(3) > 0:
-            self.shooter.shoot()
-        elif self.gamempad.getRawButton(6):
-            self.shooter.align()
-        elif self.gamempad.getRawButton(5) or self.gamempad2.getRawAxis(2) > 0:
-            self.shooter.unload()
-        elif self.gamempad.getRawAxis(2) > 0 or self.gamempad2.getRawAxis(3) > 0:
-            self.shooter.intake()
-        else:
-            self.shooter.stop()
-
-        # WoF
-        if self.gamempad2.getRawButton(3):
-            self.wof.handleFirstStage()
-        elif self.gamempad2.getRawButton(2):
-            self.wof.handleSecondStage()
-        elif self.gamempad2.getRawButton(4):
-            self.wof.reset()
-        elif self.gamempad2.getRawButton(5):
-            self.wof.manualTurn(1)
-        elif self.gamempad2.getRawButton(6):
-            self.wof.manualTurn(-1)
-        else:
-            self.wof.manualTurn(0)
-        '''
-        # Update smartdashboard
-        self.update_sd()
-
-    def update_sd(self):
-        """
-        Calls each component's own update function
-        and puts data to the smartdashboard.
-        """
-        self.sd.putNumber('Climb_Current_Draw', self.pdp.getCurrent(10))
-
-        self.drive.update_smartdash()
-        self.colorSensor.updateSD()
-        self.wof.updateSD()
-        self.vision.updateTable()
-
-if __name__ == "__main__":
-    wpilib.run(MyRobot)
+        self.swerve.drive(xSpeed, ySpeed, rot, fieldRelative, self.getPeriod())
