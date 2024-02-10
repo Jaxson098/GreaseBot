919c8f0dd9491425de086d2751ea3b0c2ee5cc67
Emerson936 <57594164+Emerson936@users.noreply.github.com>
2024-02-10 13:45:57 -0500

    Update drivetrain.py

--- drivetrain.py
+++ drivetrain.py
@@ -1,77 +1,155 @@
-#!/usr/bin/env python3
 #
 # Copyright (c) FIRST and other WPILib contributors.
 # Open Source Software; you can modify and/or share it under the terms of
 # the WPILib BSD license file in the root directory of this project.
 #
 
+import math
 import wpilib
-import wpimath
-import wpilib.drive
-import wpimath.filter
-import wpimath.controller
-import drivetrain
+import wpimath.geometry
+import wpimath.kinematics
+import swervemodule
+
+kMaxSpeed = 1.0  # meters per second
+kRMaxSpeed = 1.0
+kTMaxSpeed = 1.0
+kMaxAngularSpeed = math.pi  # 1/2 rotation per second
+frontLeftZero = 0
+frontRightZero = 0
+backLeftZero = 0
+backRightZero = 0
 
 
-class MyRobot(wpilib.TimedRobot):
-    def robotInit(self) -> None:
-        """Robot initialization function"""
-        self.controller = wpilib.Joystick(2)
-        #self.controller = wpilib.PS4Controller(2)
-        #self.controller = wpilib.XboxController(0)
-        self.swerve = drivetrain.Drivetrain()
+class Drivetrain:
+    """
+    Represents a swerve drive style drivetrain.
+    """
 
-        # Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0 to 1.
-        # Speed limiters
+    def __init__(self) -> None:
+        # NOTE: May need to tweak the center measurements 44.5mm rough measurement
+        # NOTE: EVERYTHING IS MEASURE IN METERS! 
+        # NOTE: Update center measure distance from each module
 
-        self.xspeedLimiter = wpimath.filter.SlewRateLimiter(3)
-        self.yspeedLimiter = wpimath.filter.SlewRateLimiter(1)
-        self.rotLimiter = wpimath.filter.SlewRateLimiter(0.5)
+        self.frontLeftLocation = wpimath.geometry.Translation2d(0.36, 0.36)
+        self.frontRightLocation = wpimath.geometry.Translation2d(0.36, -0.36)
+        self.backLeftLocation = wpimath.geometry.Translation2d(-0.36, 0.36)
+        self.backRightLocation = wpimath.geometry.Translation2d(-0.36, -0.36)
 
-        # Align the wheels to 0
-        #self.swerve.alignment()
+        self.frontLeft = swervemodule.SwerveModule(4, 3, 4, 13)
+        self.frontRight = swervemodule.SwerveModule(7, 8, 7, 10)
+        self.backLeft = swervemodule.SwerveModule(2, 1, 2, 11)
+        self.backRight = swervemodule.SwerveModule(5, 6, 5, 12)
 
-    #FUTURE
-    def autonomousPeriodic(self) -> None:
-        #self.driveWithJoystick(False)
-        self.swerve.updateOdometry()
+        '''
+        BERT NOTES:
+        
+        driveMotorID: int,
+        turningMotorID: int,
+        driveEncoderID: int,
+        turningEncoderID: int,
 
-    def teleopPeriodic(self) -> None:
-        self.driveWithJoystick(False)
-        #self.shootWithJoystick(False)
+        Front Left (+,+) 
+            - Drive Motor: 4
+            - Rotation Motor: 3
+            - Drive Encoder: 4
+            - Rotation Encoder: 13 
+        
+        Front Right (+,-) 
+            - Drive Motor: 7
+            - Rotation Motor: 8
+            - Drive Encoder: 7
+            - Rotation Encoder: 10
 
-    def driveWithJoystick(self, fieldRelative: bool) -> None:
-        # Get the x speed. We are inverting this because Xbox controllers return
-        # negative values when we push forward.
-        # NOTE: Check if we need inversion here
-        xSpeed = (
-            self.xspeedLimiter.calculate(
-                wpimath.applyDeadband(self.controller.getRawAxis(1), 0.1)
-            )
-            * drivetrain.kMaxSpeed
+        Rear Left (-,+) 
+            - Drive Motor: 2
+            - Rotation Motor: 1
+            - Drive Encoder: 2
+            - Rotation Encoder: 11
+
+        Rear Right (-,-) 
+            - Drive Motor: 5
+            - Rotation Motor: 6
+            - Drive Encoder: 5
+            - Rotation Encoder: 12
+
+        '''
+
+        self.gyro = wpilib.AnalogGyro(0)
+
+        #NOTE: Just defining the fixed kinematics of the bot
+        self.kinematics = wpimath.kinematics.SwerveDrive4Kinematics(
+            self.frontLeftLocation,
+            self.frontRightLocation,
+            self.backLeftLocation,
+            self.backRightLocation,
         )
 
-        # Get the y speed or sideways/strafe speed. We are inverting this because
-        # we want a positive value when we pull to the left. Xbox controllers
-        # return positive values when you pull to the right by default.
-        # NOTE: Check if we need inversion here
-        ySpeed = (
-            -self.yspeedLimiter.calculate(
-                wpimath.applyDeadband(self.controller.getRawAxis(2), 0.6)
-            )
-            * drivetrain.kTMaxSpeed
+        #NOTE: getPosition - need to determine position value - velocity and angle -
+        #NOTE: Need to understand expected units/values returned - is it meters & radians?
+        self.odometry = wpimath.kinematics.SwerveDrive4Odometry(
+            self.kinematics,
+            self.gyro.getRotation2d(),
+            (
+                self.frontLeft.getPosition(),
+                self.frontRight.getPosition(),
+                self.backLeft.getPosition(),
+                self.backRight.getPosition(),
+            ),
         )
 
-        # Get the rate of angular rotation. We are inverting this because we want a
-        # positive value when we pull to the left (remember, CCW is positive in
-        # mathematics). Xbox controllers return positive values when you pull to
-        # the right by default.
-        rot = (
-            -self.rotLimiter.calculate(
-                wpimath.applyDeadband(self.controller.getRawAxis(4), 0.5)
+        print(self.frontLeft.getPosition(), self.frontRight.getPosition(), self.backLeft.getPosition(), self.backRight.getPosition())
+
+        self.gyro.reset()
+
+    def drive(
+        self,
+        xSpeed: float,
+        ySpeed: float,
+        rot: float,
+        fieldRelative: bool,
+        periodSeconds: float,
+    ) -> None:
+        """
+        Method to drive the robot using joystick info.
+        :param xSpeed: Speed of the robot in the x direction (forward).
+        :param ySpeed: Speed of the robot in the y direction (sideways).
+        :param rot: Angular rate of the robot.
+        :param fieldRelative: Whether the provided x and y speeds are relative to the field.
+        :param periodSeconds: Time
+        """
+        swerveModuleStates = self.kinematics.toSwerveModuleStates(
+            wpimath.kinematics.ChassisSpeeds.discretize(
+                wpimath.kinematics.ChassisSpeeds.fromFieldRelativeSpeeds(
+                    xSpeed, ySpeed, rot, self.gyro.getRotation2d()
+                )
+                if fieldRelative
+                else wpimath.kinematics.ChassisSpeeds(xSpeed, ySpeed, rot),
+                periodSeconds,
             )
-            * drivetrain.kRMaxSpeed
+        )
+        wpimath.kinematics.SwerveDrive4Kinematics.desaturateWheelSpeeds(
+            swerveModuleStates, kMaxSpeed
         )
 
-        #self.swerve.drive(xSpeed, ySpeed, rot, fieldRelative, self.getPeriod())
-        self.swerve.drive(xSpeed, ySpeed, 0, fieldRelative, self.getPeriod())
+        #NOTE: Should we desaturate for Turning speed motors? 
+        
+        self.frontLeft.setDesiredState(swerveModuleStates[0])
+        self.frontRight.setDesiredState(swerveModuleStates[1])
+        self.backLeft.setDesiredState(swerveModuleStates[2])
+        self.backRight.setDesiredState(swerveModuleStates[3])
+
+    def updateOdometry(self) -> None:
+        """Updates the field relative position of the robot."""
+        self.odometry.update(
+            self.gyro.getRotation2d(),
+            (
+                self.frontLeft.getPosition(),
+                self.frontRight.getPosition(),
+                self.backLeft.getPosition(),
+                self.backRight.getPosition(),
+            ),
+        )
+    
+    def alignment(self) -> None:
+        '''Updates the wheel alignment for robot to zero'''
+        #Leverage Network Tables to report out each wheel position
