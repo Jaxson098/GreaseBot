74349802647bcc59473c12fc2f07b5cb330a2389
Shawndod3 <157923705+Shawndod3@users.noreply.github.com>
2024-02-20 18:38:11 -0500

    Update navx.py

--- navx.py
+++ navx.py
@@ -1,1 +1,6 @@
+self.gyro = AnalogGyro(0)
 
+self.gyro = ctre.WPI_PigeonIMU(0); # Pigeon is on CAN Bus with device ID 0
+# OR (choose one or the other based on your connection)
+talon = ctre.TalonSRX(0); # TalonSRX is on CAN Bus with device ID 0
+self.gyro = ctre.WPI_PigeonIMU(talon) # Pigeon uses the talon created above
