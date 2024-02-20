d97ed7dc37cc8745c4466e48faa4979767a50077
Shawndod3 <157923705+Shawndod3@users.noreply.github.com>
2024-02-20 18:46:01 -0500

    Update navx.py

--- navx.py
+++ navx.py
@@ -1,6 +1,9 @@
 self.gyro = AnalogGyro(0)
+ 
 
-self.gyro = ctre.WPI_PigeonIMU(0); # Pigeon is on CAN Bus with device ID 0
-# OR (choose one or the other based on your connection)
-talon = ctre.TalonSRX(0); # TalonSRX is on CAN Bus with device ID 0
-self.gyro = ctre.WPI_PigeonIMU(talon) # Pigeon uses the talon created above
+#navx micro for angles
+        self.angler = navx.AHRS.create_i2c()
+
+ self.armExtensionMotor = ctre.WPI_TalonSRX(ArmExtensionMotorPort)
+        self.angleMotor = ctre.WPI_TalonSRX(AngleMotorPort)
+
