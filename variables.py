b83100a31fc3a298dd258e681aeaeeb26bd1d204
Emerson936 <57594164+Emerson936@users.noreply.github.com>
2024-02-23 22:15:10 -0500

    Update variables.py

--- variables.py
+++ variables.py
@@ -5,7 +5,7 @@ import math
 import robot
 
 kMaxSpeed = 2.0  # meters per second
-kRMaxSpeed = 0.1
+kRMaxSpeed = 4.0
 kTMaxSpeed = 2.0
 kMaxAngularSpeed = math.pi  # 1/2 rotation per second
 frontLeftZero = 0
@@ -16,29 +16,15 @@ zeroThreshold = wpimath.geometry.Rotation2d(0.3)
 drivePID_P = 0.02
 drivePID_I = 0
 drivePID_D = 0
-turnPID_P = 0.04
+turnPID_P = 2 # 0.06
 turnPID_I = 0
-turnPID_D = 0.000053
-driveFF_1 = 1
+turnPID_D = 0.000053 #0.000053
+driveFF_1 = 0.7
 driveFF_2 = 3
-turnFF_1 = 0
-turnFF_2 = 0.45
+turnFF_1 = 0.3 #0.05
+turnFF_2 = 1 #0.45 
 TurnState = 0
-IntakeMotor1ID = 16
-IntakeMotor2ID = 17
-shootMotor1ID = 14
-shootMotor2ID = 15
-self.shootMotor1ID.set(1)
-self.shootMotor2ID.set(2)
-self.shootMotor1ID.set(5)
-self.shootMotor2ID.set(5) 
-self.shootMotor1ID.set(0)
-self.shootMotor2ID.set(0)
-self.IntakeMotor1ID.set(-2)
-self.IntakeMotor2ID.set(-3) 
-self.IntakeMotor1ID.set(0)
-self.IntakeMotor2ID.set(0)
-  
+
 def setTurnState(rot) -> None:
     global TurnState
     if abs(rot) > 0:
