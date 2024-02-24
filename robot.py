7ddb7d246ec69cb70d368854da3a39796401960c
rcarlin <rcarlin@eustace.org>
2024-02-24 13:57:22 -0500

    Update robot.py - # VAR on lines that appear to need vars in place of hard-coded values

--- robot.py
+++ robot.py
@@ -20,7 +20,7 @@ import navxGyro
 class MyRobot(wpilib.TimedRobot):
     def robotInit(self) -> None:
         """Robot initialization function"""
-        self.controller = wpilib.Joystick(2)
+        self.controller = wpilib.Joystick(2) # VAR
         self.swerve = drivetrain.Drivetrain()
         self.shooter = shooter.ShootModule()
         # navxGyro is a file to test the navx Gyro. This can be ignored/commented out.
@@ -29,9 +29,9 @@ class MyRobot(wpilib.TimedRobot):
         # Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0 to 1.
         # Speed limiters
 
-        self.xspeedLimiter = wpimath.filter.SlewRateLimiter(3)
-        self.yspeedLimiter = wpimath.filter.SlewRateLimiter(3)
-        self.rotLimiter = wpimath.filter.SlewRateLimiter(3)
+        self.xspeedLimiter = wpimath.filter.SlewRateLimiter(3) # VAR
+        self.yspeedLimiter = wpimath.filter.SlewRateLimiter(3) # VAR
+        self.rotLimiter = wpimath.filter.SlewRateLimiter(3) # VAR
 
         # Align the wheels to 0
         #self.swerve.alignment()
@@ -46,10 +46,10 @@ class MyRobot(wpilib.TimedRobot):
         self.navxGyro.getGyro()
         #self.shootWithJoystick(False)
         #self.shooter.speakershootmotor(1, 1)
-        if self.controller.getRawButton(1) == 1:
+        if self.controller.getRawButton(1) == 1: # VAR
             print("square button pressed")
             self.swerve.alignment()
-        if self.controller.getRawButton(4) == 1:
+        if self.controller.getRawButton(4) == 1: # VAR
             self.swerve.drive(0,0,0,0,self.getPeriod())
             self.swerve.alignment()
 
@@ -59,7 +59,7 @@ class MyRobot(wpilib.TimedRobot):
         # NOTE: Check if we need inversion here
         xSpeed = (
             self.xspeedLimiter.calculate(
-                wpimath.applyDeadband(self.controller.getRawAxis(1), 0.1)
+                wpimath.applyDeadband(self.controller.getRawAxis(1), 0.1) # VAR
             )
             * variables.kMaxSpeed
         )
@@ -70,7 +70,7 @@ class MyRobot(wpilib.TimedRobot):
         # NOTE: Check if we need inversion here
         ySpeed = (
             -self.yspeedLimiter.calculate(
-                wpimath.applyDeadband(self.controller.getRawAxis(2), 0.6)
+                wpimath.applyDeadband(self.controller.getRawAxis(2), 0.6) # VAR
             )
             * variables.kTMaxSpeed
         )
@@ -81,11 +81,11 @@ class MyRobot(wpilib.TimedRobot):
         # the right by default.
         rot = (
             (-self.rotLimiter.calculate(
-                wpimath.applyDeadband(self.controller.getRawAxis(3), 0.2)
+                wpimath.applyDeadband(self.controller.getRawAxis(3), 0.2) # VAR
             )
             * variables.kRMaxSpeed) +
             (self.rotLimiter.calculate(
-                wpimath.applyDeadband(self.controller.getRawAxis(4), 0.2)
+                wpimath.applyDeadband(self.controller.getRawAxis(4), 0.2) # VAR
             )
             * variables.kRMaxSpeed)
         )
