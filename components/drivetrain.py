a345a42035747fe20b0256deac8ab313b9f31393
Jaxson Paige <jaxsonpaige@Jaxsons-MacBook-Air.local>
2024-03-28 17:27:48 -1000

    fixed IDs

--- components/drivetrain.py
+++ components/drivetrain.py
@@ -25,10 +25,10 @@ class Drivetrain:
     def __init__(self) -> None:
 
         #adjusted to our IDs
-        self.frontLeft = swervemodule.SwerveModule(9, 2, 9, 12)
-        self.frontRight = swervemodule.SwerveModule(3, 8, 3, 13)
-        self.backLeft = swervemodule.SwerveModule(7, 5, 7, 11)
-        self.backRight = swervemodule.SwerveModule(4, 6, 4, 10)
+        self.frontLeft = swervemodule.SwerveModule(7, 2, 5, 12)
+        self.frontRight = swervemodule.SwerveModule(3, 4, 3, 13)
+        self.backLeft = swervemodule.SwerveModule(9, 6, 9, 11)
+        self.backRight = swervemodule.SwerveModule(8, 2, 8, 10)
 
         self.kinimaticsLocation()
 
